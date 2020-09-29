d1c11e3a360a938e66f360b091c6fd0a1501b7f6
vahid abdi <itsvahidabdi@gmail.com>
2020-09-29 12:27:36 +0330

    added webpack.mix.js tailwind.config.js and app css/js files to src folder

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [
+      './*.php',
+      './templates/**/*.twig'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
